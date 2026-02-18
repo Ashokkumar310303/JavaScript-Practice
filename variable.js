533c8c4ab215ad8ebb74f7841f7cf8cfa8f0883d
Ashok kumar <116782284+Ashokkumar310303@users.noreply.github.com>
2026-02-18 08:25:03 +0000

    practice

---
+++ variable.js
@@ -0,0 +1,9 @@
+const constant=1234;    //it's constant doesn't change once give
+
+let value1=2;
+var value2=3;
+
+value3="hello";
+let value4; 
+
+console.table([constant,value1,value2,value3,value4]);
